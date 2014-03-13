8645eedff416459b7ea2dc89ee52a405bd8a80be
scotopic <scotopic@users.noreply.github.com>
2014-03-13 16:54:20 -0400

    readding changes back

--- BigNerdsPaint/BNRCanvasViewController.m
+++ BigNerdsPaint/BNRCanvasViewController.m
@@ -22,6 +22,14 @@
         UIBarButtonItem *saveBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveImage:)];
         self.navigationItem.rightBarButtonItem=saveBtn;
         
+        UIBarButtonItem *undoBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemUndo target:self action:@selector(undoButtonClicked:)];
+        
+        self.navigationItem.rightBarButtonItem=undoBtn;
+        
+        UIBarButtonItem *redoBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRedo target:self action:@selector(redoButtonClicked:)];
+        
+        self.navigationItem.rightBarButtonItems = @[saveBtn, redoBtn, undoBtn];
+
     }
     return self;
 }
